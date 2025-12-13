77ec98237f3bffaad544ac361d63349adb03e876
muhammadasif46 <asifahmed32c@gmail.com>
2025-12-13 15:48:55 +0500

    sat class update code

---
+++ JAVASCRIPT/class-05-chapter-10-to-12-if-statements/app.js
@@ -0,0 +1,57 @@
+// console.log("hello");
+
+// Chapter: 10 - If Statement
+
+// console.log("1");
+
+// var city = "karachi"
+// if (city == "karachi") {
+//     console.log("2");
+// } else {
+//     console.log("kuch bhi nahi..");
+// }
+// console.log("3");
+
+
+
+// Chapter: 11 - Comparsion operator
+
+// > greater than
+// < less than
+// >= greater than equal
+// <= less than equal
+// == equal - check only value
+// != not equal - check only value
+// === equal - check both value and type
+// !== not equal - check both value and type
+
+// Data Types: (Javascript)
+
+// 4234325;        // number type
+// true;           // boolean type
+// "bdvshshvhsd";  //string type
+
+// console.log("1");
+// var age = 12
+// if (age != 12) {
+//     console.log("2");
+// } else {
+//     console.log("kuch bhi nahi..");
+// }
+// console.log("3");
+
+// Assignment 02: PDF --> 6-to-9 - (Q:5)
+
+// // Solutions
+var table = +prompt("Enter your fav number?");
+
+document.write( table + " * 1 = " + table * 1 + "<br />")
+document.write( table + " * 2 = " + table * 2 + "<br />")
+document.write( table + " * 3 = " + table * 3 + "<br />")
+document.write( table + " * 4 = " + table * 4 + "<br />")
+document.write( table + " * 5 = " + table * 5 + "<br />")
+document.write(table + " * 6 = " + table * 6 + "<br />")
+document.write(table + " * 7 = " + table * 7 + "<br />")
+document.write(table + " * 8 = " + table * 8 + "<br />")
+document.write(table + " * 9 = " + table * 9 + "<br />")
+document.write( table + " * 10 = " + table * 10)
