3b8e0003da462a9f7b1d1c3346d77aa9c3664f68
muhammadasif46 <asifahmed32c@gmail.com>
2025-12-20 18:59:03 +0500

    update today class code

---
+++ JAVASCRIPT/class-06-chapter-13-to14/app.js
@@ -0,0 +1,116 @@
+// console.log("hello");
+
+// var object = prompt("Enter object??")
+
+// if(object == "Chair"){
+//     console.log("Chair lekay ajao..");
+// } else if(object == "Dice"){
+//     console.log("Dice lekay ajao..");
+// } else if(object == "Duster"){
+//     console.log("Duster lekay ajao..");
+// } else {
+//     console.log("Wapis Ajaoo..");
+// }
+
+
+// var value = prompt("enter a value")
+// console.log(value);
+
+// var firstNo = +prompt("enter a value")
+// var secondNo = +prompt("enter a value")
+// var total = firstNo + secondNo
+// console.log(total);
+
+// &&  Right and Left side both are true
+// ||  Right or Left side any one is true
+
+// var language = prompt("Enter Your Langauge?")
+// var loc = prompt("Enter Your Location?")
+// var exp = prompt("Enter Your experience?")
+
+// if(language == "html" && loc == "karachi" && exp == "1 year"){
+//     console.log("You are Hired!");
+// } else {
+//     console.log("You are not Hired!");
+// }
+
+
+// if(language == "html" || loc == "karachi" || exp == "1 year"){
+//     console.log("You are Hired!");
+// } else {
+//     console.log("You are not Hired!");
+// }
+
+
+// true && true
+// true
+// true && false
+// false
+// false && true
+// false
+// false && false
+// false
+
+
+// true || true
+// true
+// true || false
+// true
+// false || true
+// true
+// false || false
+// false
+
+
+// 14
+
+// var food = prompt("Enter your food ?")
+// var extra = "Salad"
+
+// if(food == "Biryani"){
+//     if(extra == "Raita"){
+//         console.log("dono lekay ajaoo....");
+//     } else {
+//         console.log("sirf biryani lekay ajaoo....");
+//     }
+// } else if(food == "Burger"){
+//     console.log("2 burger lekay ajao..");
+// } else if (food == "Pasta"){
+//     console.log("1 lekay ajao..");
+// } else {
+//     console.log("wapis mat ana...");
+// }
+
+
+
+
+
+// var value = prompt("Enter")
+// console.log(value);
+
+// var firstNo = +prompt("Enter")
+// var secondNo = +prompt("Enter")
+// var total = firstNo + secondNo
+// console.log(total);
+
+
+// var lang = prompt("Enter your language")
+// var loc = prompt("Enter your location")
+// var exp = prompt("Enter  your experience")
+
+// if(lang == "html"){
+//     if(loc == "karachi"){
+//         if(exp == "1 year"){
+//             console.log('You Are Hired!!');
+//         } else {
+//             console.log("Apna Exp pehly 1 saal ka krky ajao..");
+//         }
+//     }
+// } else {
+//     console.log("You are not hired");
+// }
+
+
+
+
+
---
+++ JAVASCRIPT/class-07-revision-class/app.js
@@ -0,0 +1,54 @@
+// console.log("hello");
+
+// alert("hello")
+
+'dkjbcsjidb' // String
+36454376532  // NUmber
+true/false   // Boolean
+
+
+// var user = "Smith"
+// alert(user)
+// user = "Clerk"
+// alert(user)
+
+
+// var age = 11
+// // alert(age)
+// age = 14 + 2
+// alert(age)
+
+
+// var dha_Phase_Two = "gvdhkavkfc" 
+
+
+// var a = 10
+// var b = 6
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a % b)
+
+// var a = 3
+// a = a + 2
+// console.log(a);
+
+
+// var a = 3
+// a++ // post increment
+// a-- // post decrement
+// --a // pre decre
+// ++a // pre incre
+// console.log(a);
+
+// var firstName = "Smith"
+// var lastName = "Clerk"
+// console.log("Welcome " + firstName + " " + lastName);
+
+
+// var username = prompt("Enter your name?")
+// console.log(username);
+// var age = +prompt("Enter your age?")
+// console.log(age);
+
