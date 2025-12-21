d6580b6a9899d165f56ab441ae49e968ae049462
muhammadasif46 <asifahmed32c@gmail.com>
2025-12-21 19:10:32 +0500

    update today class code

---
+++ JAVASCRIPT/class-08-revision-class-part-2/app.js
@@ -0,0 +1,139 @@
+// console.log("hello");
+
+// alert("hello")
+
+"dkjbcsjidb"; // String
+36454376532; // NUmber
+true / false; // Boolean
+
+// var user = "Smith"
+// alert(user)
+// user = "Clerk"
+// alert(user)
+
+// var age = 11
+// // alert(age)
+// age = 14 + 2
+// alert(age)
+
+// var dha_Phase_Two = "gvdhkavkfc"
+
+// var a = 10
+// var b = 6
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a % b)
+
+// var a = 3
+// a = a + 2
+// console.log(a);
+
+// var a = 3
+// a++ // post increment
+// a-- // post decrement
+// --a // pre decre
+// ++a // pre incre
+// console.log(a);
+
+// var firstName = "Smith"
+// var lastName = "Clerk"
+// console.log("Welcome " + firstName + " " + lastName);
+
+// var username = prompt("Enter your name?")
+// console.log(username);
+// var age = +prompt("Enter your age?")
+// console.log(age);
+
+// var a = 4
+
+// if(a != 4){
+//     console.log("Running-1...");
+// } else if (a > 4){
+//     console.log("Running-2...");
+// } else if (a == 4){
+//     console.log("Running-3...");
+// } else {
+//     console.log("Not Running...");
+// }
+
+// chapter 13:
+// &&
+// ||
+
+// var num = 6;
+
+// if (num <= 4 || num == 4) {
+//   console.log("Running..");
+// } else {
+//   console.log("Not Running..");
+// }
+
+
+
+// var num = 6;
+// var num2 = 3
+
+// if (num >= 4 || num == 4) {
+//     if(num2 == 3){
+//         console.log("Running..");
+//     } else {
+//         console.log("num2 is not running..");
+//     }
+// } else {
+//   console.log("Not Running..");
+// }
+
+
+
+// Arrays:
+
+// push() -> add last index 
+// pop()  -> remove last index
+
+// var std1 = "Smith"
+// var std2 = "Clerk"
+// var std3 = "Elia"
+// console.log(std1);
+
+
+
+// var students = ["Smith", "Clerk", "Elia"] 
+// var mixedArr = [1, true, "karachi", 2, false] 
+// console.log(students);
+// console.log(students[0]);
+// console.log(mixedArr[4]);
+
+
+// var cities = [];
+
+// cities[0] = "karachi"
+// cities[1] = "lahore"
+// console.log(cities);
+
+// var cities = ["karachi", "karachi","karachi","karachi","karachi","karachi","karachi","karachi","karachi","karachi","karachi","karachi","karachi"]
+
+// cities[13] = "lahore"
+// console.log(cities);
+
+// var cities = ['karachi', 'lahore']
+// cities.push('islamabad', 'multan')
+// cities.pop()
+// console.log(cities);
+
+
+
+// ==== chapter - 01 (Q-1)
+
+// alert("welcome")
+
+// ==== chapter - 01 (Q-2)
+
+// alert("Error!")
+
+// ==== chapter - 01 (Q-3)
+
+// alert("Hello \n world")
+
+// ==== chapter - 02 (Q-1)
