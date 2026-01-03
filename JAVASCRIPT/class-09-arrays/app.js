20cc81eee63644668220dac54f69074f43fa2ed6
muhammadasif46 <asifahmed32c@gmail.com>
2026-01-04 02:20:11 +0500

    update today class code

---
+++ JAVASCRIPT/class-09-arrays/app.js
@@ -0,0 +1,29 @@
+// console.log("hello")
+
+// Array methods:
+
+// Push() -> add value last index
+// Pop() -> remove value last index
+// unShift() -> add value first index
+// shift() -> remove value first index
+// splice() -> add, remove, update 
+// slice() -> copy elements
+
+
+// var students = ["Jhon","Smith","Clerk"]
+// console.log(students[2]);
+
+// students.push("Elia","ek or")
+// students.pop()
+// students.pop()
+// students.unshift("Person")
+// students.shift()
+
+// console.log(students);
+
+// var cities = ["Karachi", "Multan", "Lahore","Pindi"]
+// cities.splice(1, 2)
+// cities.splice(0,0,"Isl","Faislabad")
+// var extractedCity = cities.slice(0, 3) // index, length
+// console.log(cities);
+// console.log(extractedCity);
