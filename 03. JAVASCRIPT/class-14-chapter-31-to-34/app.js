2b28defb42c3fd36e920e48a98bbd04e96473289
muhammadasif46 <asifahmed32c@gmail.com>
2026-02-01 14:48:12 +0500

    code update

---
+++ 03. JAVASCRIPT/class-14-chapter-31-to-34/app.js
@@ -0,0 +1,32 @@
+// Javascript Date and Time:
+
+// var date = new Date()
+// console.log(date);
+// var days = ['sun', 'mon','tue','wed','thr','fri','sat'];
+// console.log(days[date.getDay()]);
+// console.log(date.getDay());
+// console.log(date.getDate());
+// console.log(date.getTime());
+
+// var firstRamadan = new Date("February 18, 2026")
+// var today = new Date()
+// var firstRamadanGetTime = firstRamadan.getTime()
+// var todayGetTime = today.getTime()
+// var diff = firstRamadanGetTime - todayGetTime
+// var finalRes = diff / (1000 * 60 * 60 * 24)
+// console.log(Math.floor(finalRes) + " days are remaining for Ramadan");
+
+// console.log(firstRamadanGetTime - todayGetTime);
+
+// var myDate = new Date("March 23, 2009");
+// var today = new Date()
+// var res = today.getFullYear() - myDate.getFullYear()
+// console.log(res);
+
+// var num = 5;
+
+// if(num < 0){
+//     num = -num
+// }
+
+// console.log(num);
---
+++ 03. JAVASCRIPT/class-15-chapter-35-to-38/app.js
@@ -0,0 +1,48 @@
+// Javascript: Functions
+
+// console.log("1");
+// function foo(){
+//     console.log("running..");
+// }
+
+// foo()
+// console.log("2");
+
+
+// function sum(a, b){ // parameters
+//     console.log(a + b);
+// }
+// sum(2, 4) // Arguments
+// sum(8, 2) // Arguments
+// sum()
+// sum()
+
+// alert("hello")
+
+// function greet(abc){
+//     console.log(abc);  
+// }
+
+// greet("Welcome!")
+
+
+// var user = prompt()
+// console.log(user);
+
+
+// function mul(a, b){
+//     // console.log("hello");
+//     return a * b;
+// }
+
+// var amount = mul(3,5)
+// console.log(amount);
+
+// console.log(a); // hello
+// var a = "hello"
+// function foo(){
+//     var a = "world"
+//     console.log(a); // world
+// }   
+// foo()
+// console.log(a); // hello 
