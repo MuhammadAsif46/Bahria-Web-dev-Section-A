6f15da776c847cfecf5c85771954b790f2b2a5f8
muhammadasif46 <asifahmed32c@gmail.com>
2026-04-19 12:39:10 +0500

    today class js revision part-01

---
+++ 03. JAVASCRIPT/class-16-revision-classes-part-01/app.js
@@ -0,0 +1,29 @@
+// ------ JS CHAPTER REVISION 1 TO 5 ------ //
+
+// alert("Hello World!");
+
+// var firstname = "Asif"
+// firstname = "Ahmed"
+// console.log(firstname);
+
+// synchronous vs asynchronous
+
+// var age = 12
+// var age;
+// console.log(age);
+// age = 21
+// console.log(age);
+// console.log(num);
+
+// var num;
+// var 1num;
+// var num1;
+// var user name;
+// var userName;
+// var $name;
+
+// var num = "2" + 6
+// var num = "2" - 6
+// var num = "2" * 6
+// var num = "2" / 6
+// console.log(num);
