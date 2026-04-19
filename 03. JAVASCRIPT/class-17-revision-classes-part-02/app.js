8c4794af8f0c47f3ce111929c1cd060b63757d45
muhammadasif46 <asifahmed32c@gmail.com>
2026-04-19 12:39:49 +0500

    today class JS revision part-02

---
+++ 03. JAVASCRIPT/class-17-revision-classes-part-02/app.js
@@ -0,0 +1,46 @@
+// ------ JS CHAPTER REVISION 6 TO 10 ------ //
+
+
+// post increment
+// pre increment
+// post decrement
+// pre decrement
+
+// var a = 3
+// console.log(a++); //3
+// console.log(a--); //4
+// console.log(a); //3
+// console.log(--a); //2
+// console.log(a); //2
+
+// a++
+// a--
+// --a
+// ++a
+
+// var a = 2
+// var b = 1
+// var c = a++ - --b + ++a + b++
+// //      3   -  0   +  4  + 1
+// //      2   -  0   +  4  + 0
+// console.log(c);
+
+// var firstName = "Hamza"
+// var lastName = "Ali"
+
+// console.log("Welcome "+ firstName + " " + lastName);
+
+// var user = prompt("Enter Your Name")
+// var user = +prompt("Enter Your Age")
+// console.log(user + 5);
+
+var num = 3
+
+if (num > 2) {
+  console.log("1");
+}
+
+
+
+
+
