342f5fc5854e6275fdd5b869ab2115d0967708ec
muhammadasif46 <asifahmed32c@gmail.com>
2026-04-26 15:14:34 +0500

    JS Revison class part-04

---
+++ 03. JAVASCRIPT/class-19-revision-classes-part-04/app.js
@@ -0,0 +1,45 @@
+// ------ JS CHAPTER REVISION 18 TO 25 ------ //
+
+// for (var i = 0; i < 10; i++) {
+//   console.log(1);
+//   if (4 == 4) {
+//     break;
+//   }
+// }
+
+// var arr = ["khi","lhr","isl","mul","hello"]
+// var found = "no"
+
+// for (var i = 0; i < arr.length; i++) {
+// //   console.log(arr[i]);
+//   if(arr[i] === "isl"){
+//     found = "yes"
+//     break;
+//   }
+// }
+
+// if(found === "yes"){
+//     console.log("Islamabad agaya hai..");
+// } else {
+//     console.log("Islamabad nahi aya..");
+// }
+
+// var country = ["pak", "china", "iran", "iraq"];
+// var cities = ["khi", "bij", "tehran", "isl"];
+
+// for (var i = 0; i < country.length; i++) {
+//   for (var j = 0; j < cities.length; j++) {
+//     console.log(country[i], cities[j]);
+//   }
+// }
+
+// Pak, khi
+// china, bij
+
+// var firstname = "KARAN"
+// console.log(firstname.toUpperCase());
+// console.log(firstname.toLowerCase());
+
+// var firstChar = firstname.slice(0, 1).toUpperCase()
+// var lastChar = firstname.slice(1).toLowerCase()
+// console.log(firstChar + lastChar);
