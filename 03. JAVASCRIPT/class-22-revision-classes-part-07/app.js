91afd25d59c526b83e623a65542789281af24a51
muhammadasif46 <asifahmed32c@gmail.com>
2026-05-18 11:41:45 +0500

    JS revision class part-07

---
+++ 03. JAVASCRIPT/class-22-revision-classes-part-07/app.js
@@ -0,0 +1,49 @@
+// ------ JS CHAPTER REVISION 35 TO 38 ------ //
+
+// console.log("1");
+
+// function greet(){
+//     console.log("Welcome");
+
+// }
+
+// greet()
+// greet()
+// console.log("2");
+
+// function greet(a){ //parameters
+//     console.log(a);
+
+// }
+
+// greet("Hamza") // argument
+// greet("Jhon") // argument
+
+//  function sum(x, y){
+//     console.log(x + y);
+
+//  }
+
+//  sum(2, 3 , 6)
+//  sum(12, 13)
+
+// var a = "hello"
+
+// function foo(){
+//     var b = "world"
+//     console.log(a);
+
+// }
+// console.log(b);
+// foo().
+
+function foo(a, b) {
+  var c = a + b;
+  return  c;
+}
+
+var user = foo(1, 2);
+console.log(user);
+
+// var user = prompt()
+// console.log(user);
