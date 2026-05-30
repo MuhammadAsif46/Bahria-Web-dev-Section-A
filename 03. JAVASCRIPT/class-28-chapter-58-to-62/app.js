3caf8b0d990ff4c7e3d8692b5a147fbe9ad0569f
muhammadasif46 <asifahmed32c@gmail.com>
2026-05-30 13:10:22 +0500

    JS class cover 4 chapters

---
+++ 03. JAVASCRIPT/class-28-chapter-58-to-62/app.js
@@ -0,0 +1,13 @@
+// ------ JS CHAPTER 58 TO 62 ------ //
+
+// console.log(document.childNodes[1].childNodes[2].childNodes[1]);
+// var heading = document.childNodes[1].childNodes[2].childNodes[1];
+// heading.style.backgroundColor = "red"
+
+ var div2 = document.getElementById("div2");
+ console.log(div2.firstChild.style.color = "blue");
+ console.log(div2.lastChild.style.color = "green");
+ console.log(div2.childNodes[1].style.color = "pink");
+ console.log(div2.parentNode.style.backgroundColor = "black");
+ 
+// var pNode = kidNode.parentNode
