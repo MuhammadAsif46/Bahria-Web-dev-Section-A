fb66b99d0eba99008e3b40949b2036fdec8c8ee9
muhammadasif46 <asifahmed32c@gmail.com>
2026-06-15 23:53:54 +0500

    JS class cover 2 chapters

---
+++ 03. JAVASCRIPT/class-31-chapter-69-and-70/app.js
@@ -0,0 +1,57 @@
+// ------ JS CHAPTER 69 TO 70 ------ //
+
+// objects
+
+// create
+// add
+// update
+// get
+// delete
+ 
+// var student1 = {
+//   stdName: "Jhon",
+//   fatherName: "Smith",
+//   age: 13,
+//   class: "6th",
+// }
+
+// var student2 = {
+//   stdName: "Smith",
+//   fatherName: "Clerk",
+//   age: 31,
+//   class: "16th",
+// }
+// student2.age = 35
+// student2.email = "smith@gmail.com"
+
+// delete student2.class
+// console.log(student2);
+
+
+// var a = "hello"
+// a = "world" 
+
+var car = {
+  carName: "Civic",
+  price: 80_00_000,
+  isAuto: true,
+  colors: ["White", "Black", "Red"],
+  otherDetails: {
+    door: 2,
+    wheels: 6,
+  }
+}
+
+console.log(car);
+console.log(car.carName);
+console.log(car.colors[1]);
+console.log(car.otherDetails.wheels);
+
+
+
+// console.log(student1.stdName);
+// console.log(student1.age);
+// console.log(student1["age"]);
+// console.log(student1["class"]);
+
+
