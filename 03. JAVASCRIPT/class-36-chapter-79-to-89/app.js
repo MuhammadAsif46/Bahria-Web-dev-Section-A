8535e32e999ea63e073ef4dd50efda128779ce80
Muhammad Asif <asifahmed32c@gmail.com>
2026-06-28 21:49:15 +0500

    Javascript book end

---
+++ 03. JAVASCRIPT/class-36-chapter-79-to-89/app.js
@@ -0,0 +1,37 @@
+// ====== Chapter no 79 to 89 ====== //
+
+// function openWindow(){
+//    window.open("home.html", "", "width=420,height=380,left=400,top=100")
+
+//    if(cnic.value === "^0-9{5}-[0-9]{7}-[0-9]{1}$"){
+//       alert("Its working fine")
+//    } else {
+//       alert("Please enter correct cnic number")
+//    }
+
+//    if(zipCode.value === "^\d{5}$"){
+//       alert("Its working fine")
+//    } else {
+//       alert("Please enter correct Zip number")
+//    }
+// }
+
+
+console.log("hello");
+
+try {  
+   aler("Pakistan")
+} catch(err){
+   console.log("aler spelling wrong");
+}
+
+try {  
+   aler("Pakistan")
+   throw "Please use correct Javascript Syntax"
+} catch(err){
+   console.log(err);
+   
+}
+
+console.log("world");
+
