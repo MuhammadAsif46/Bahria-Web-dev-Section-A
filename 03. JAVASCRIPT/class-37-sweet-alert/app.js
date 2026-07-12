8e81b2d535ec97b2cdc4cb1e9a7c9cd949e0b294
muhammadasif46 <asifahmed32c@gmail.com>
2026-07-12 13:04:21 +0500

    js class sweet alert

---
+++ 03. JAVASCRIPT/class-37-sweet-alert/app.js
@@ -0,0 +1,13 @@
+// alert("Hello")
+
+function showAlert() {
+    Swal.fire({
+        // title: "Hello World",
+        // text: "Pakistan, Karachi",
+        // icon: "question"
+        imageUrl: "https://picsum.photos/200/300",
+        imageHeight: 300,
+        imageWidth: 300,
+        imageAlt: "A tall image"
+    });
+}
