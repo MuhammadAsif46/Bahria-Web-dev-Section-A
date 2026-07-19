943ff276e962ea50abfe4a04a94181aaaf2ceaa0
muhammadasif46 <asifahmed32c@gmail.com>
2026-07-19 14:37:46 +0500

    Advance JS Topics Part 01

---
+++ 04. ADVANCE-JAVASCRIPT/01. class-one/app.js
@@ -0,0 +1,69 @@
+// ====== ADVANCE JAVASCRIPT PART 01 ====== //
+
+// -------- VAR vs LET vs CONST ------- //
+
+// 1. var
+// 2. let
+// 3. const
+
+// var abc = "hello"
+
+// function greet(){
+//     var abc = "hello"
+//     console.log(abc);
+// }
+
+// console.log(abc); // not defined
+
+// if(true){
+//     const abc = "school"
+// }
+
+// console.log(abc);
+
+
+// const abc = "school"
+// console.log(abc);
+
+// abc = "college"
+
+// console.log(abc);
+
+// var abc = "hello"
+
+// var abc = "world"
+// console.log(abc);
+
+
+// const abc = "hello"
+
+// const abc = "world"
+
+// console.log(abc);
+
+// ---------- Template literals ----------- //
+
+// let firstname = "Clerk"
+// let lastname = "Morgan"
+// let city = "Karachi"
+
+// // let fullname = "Welcome, " + firstname + " " + lastname + " from " + city 
+// let fullname = `Welcome, ${firstname} ${lastname} from ${city}`
+// console.log(fullname);
+
+
+// ---------- Ternary Operator ----------- //
+
+// var num = 5
+
+// if(num == 5){
+//     console.log("number is equal to 5");
+// } else {
+//     console.log("number is not equal to 5");
+// }
+
+// let num = 5 
+
+// num != 5 ?  : console.log("number is not equal to 5")
+let age = 18 
+age >= 18 ? "true" : "false"
