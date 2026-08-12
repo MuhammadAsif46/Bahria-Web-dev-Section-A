810965c8fe76308e41920518863dc011349b8210
muhammadasif46 <asifahmed32c@gmail.com>
2026-08-12 10:19:11 +0500

    Advance JS Topic part 4

---
+++ 04. ADVANCE-JAVASCRIPT/04. class-four/app.js
@@ -0,0 +1,23 @@
+// ====== ADVANCE JAVASCRIPT PART 04 ====== //
+
+// forEach()
+// map()
+// filter()
+// find()
+// some()
+// every()
+// reduce()
+// sort()
+// join()
+// reverse()
+// split()
+
+let fruits = ["Mango", "Apple", "Orange", "Kiwi"]
+
+// for(var i = 0; i < fruits.length; i++){
+//     console.log(fruits[i]);
+// }
+
+fruits.forEach(function(v){
+    console.log(v);
+})
