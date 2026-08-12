efcba8d75cff3ea76a3192b3eb2f067b88c846fb
muhammadasif46 <asifahmed32c@gmail.com>
2026-08-12 10:20:33 +0500

    Advance JS Topic part 6

---
+++ 04. ADVANCE-JAVASCRIPT/06. class-six/app.js
@@ -0,0 +1,8 @@
+// ====== ADVANCE JAVASCRIPT PART 06 ====== //
+
+
+import { username, age, sum } from "./mian.js"
+
+console.log(username);
+console.log(age);
+console.log(sum(3, 7));
---
+++ 04. ADVANCE-JAVASCRIPT/06. class-six/mian.js
@@ -0,0 +1,9 @@
+let username = "Jhon Elia"
+let age = 45
+let sum = (a, b) => a + b
+
+export {
+    username,
+    age,
+    sum
+}
